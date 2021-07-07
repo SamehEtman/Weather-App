a2132189fd4a5f7f4abab856f947fb058d46e8d9
Sameh <samehothman20162017@gmail.com>
2021-07-08 00:08:17 +0200

    completing the project

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const request = require('request')
+const geoCode = require('./utils/geoCode.js')
+const foreCast = require('./utils/foreCast.js')
 
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=cairo&units=metric&appid=886bb38efe659c46eac3825623ae4435'
 
-request({url : url , json: true} , (error , responce) =>{
+
+
+const address = process.argv[2]
+if (!address) return console.log('Enter an adress name')
+
+const getData = geoCode.getGeoCode(address, (error , geoCodeData) =>{
     if (error){
-        console.log('Unable to connect to the weather services!');
-    }else if (responce.body.message){
-        console.log(responce.body.message)
-    }else{
-        console.log('The Temp in out beautiful city : ' , responce.body.name)
-        console.log(responce.body.main.temp)
+        return console.log(error);
     }
+
+    foreCast.getForeCast(geoCodeData , (error , foreCastData)=>{
+        if (error)
+            return console.log(error)
+        
+        console.log('City : ' , foreCastData.name);
+        console.log('Descriptrion : ' , foreCastData.descripe)
+        console.log('Temperature : ' , foreCastData.temp)
+    })
 })
 
 
-const geoCodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/tanta.json?access_token=pk.eyJ1Ijoic2FtZWg5OCIsImEiOiJja3F0Znd1aXowN25jMm9vMmxyaW0zajU3In0.eXJf1wSUAFHVmiiH5rTNDw&limit=1'
-request({url:geoCodeURL , json:true} , (error , responce) =>{
-    if (error){
-        console.log('Unable to connect to location service!')
-    }else if (responce.body.features.length === 0){
-        console.log("Cant't find location!")
-    }else{
-        console.log('the lat and lon of our beloved city ' ,
-        responce.body.features[0].place_name , ' : ')
-        console.log('lat' , responce.body.features[0].center[0])
-        console.log('lon' , responce.body.features[0].center[1])
-    }
-    
 
-})
+
+
+
+
+
+
+
+
+
